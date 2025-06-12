2f863290fe4dec6116e0a206d3001d892cfd1009
MalenaMC <153699639+MalenaMC@users.noreply.github.com>
2025-06-12 10:57:59 -0500

    Actualización tabla

--- src/app/student/panel/dashboard/dashboard.component.ts
+++ src/app/student/panel/dashboard/dashboard.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
 import { PanelHeaderComponent } from '../../../components/dashboard/shared-components/panel-header/panel-header.component';
 import { SubjectGradeBarComponent } from '../student-component/subject-grade-bar/subject-grade-bar.component';
-
-interface Assignment {
-  no: string;
-  task: string;
-  subject: string;
-  dueDate: string;
-  status: 'In Progress' | 'Completed' | 'Not Started';
-}
+import { TableComponent } from '../../../components/table/table.component';
+import { Assignment } from '../../services/assignments.service';
 
 interface SubjectGrade {
   subject: string;
-  progress: number;
+  currentScore: number;
+  maxScore: number;
   color: string;
 }
 
@@ -25,15 +20,15 @@ interface Announcement {
 
 @Component({
   selector: 'app-dashboard',
-  imports: [PanelHeaderComponent, SubjectGradeBarComponent],
+  imports: [PanelHeaderComponent, SubjectGradeBarComponent, TableComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  currentDate = new Date();
   attendancePercentage = 80;
   
-  subjectGrades = [
+  //PROGRESO Y CALIFICACIÓN POR CURSO
+  subjectGrades: SubjectGrade[] = [
     { subject: 'Aritmética', currentScore: 18, maxScore: 20, color: '#FFED9F' },
     { subject: 'Biología', currentScore: 16, maxScore: 20, color: '#c4b5fd' },
     { subject: 'Anatomía', currentScore: 19, maxScore: 20, color: '#FFCED0' },
@@ -41,6 +36,7 @@ export class DashboardComponent {
     // ... más materias
   ];
 
+  //ANUNCIOS
   announcements: Announcement[] = [
     {
       title: 'Sports Day Announcement',
@@ -56,18 +52,38 @@ export class DashboardComponent {
     }
   ];
 
-  assignments: Assignment[] = [
-    { no: '01', task: 'Read Chapters 1-3', subject: 'English', dueDate: '12 May 2024', status: 'In Progress' },
-    { no: '02', task: 'Complete Problem Set #5', subject: 'Maths', dueDate: '12 May 2024', status: 'Not Started' },
-    { no: '03', task: 'Write Lab Report on Acid-Base Titration', subject: 'Physics', dueDate: '12 May 2024', status: 'In Progress' },
-    { no: '04', task: 'Prepare for Oral Presentation', subject: 'Chemistry', dueDate: '12 May 2024', status: 'In Progress' },
-    { no: '05', task: 'Create Art Piece for Final Project', subject: 'English', dueDate: '12 May 2024', status: 'Completed' },
-    { no: '06', task: 'Write Research Paper on Climate Change', subject: 'EVS', dueDate: '12 May 2024', status: 'In Progress' },
-    { no: '07', task: 'Complete Math Quiz on Algebra', subject: 'Math', dueDate: '12 May 2024', status: 'Completed' },
-    { no: '08', task: 'Prepare for History Class Debate', subject: 'History', dueDate: '12 May 2024', status: 'Not Started' },
-    { no: '09', task: 'Submit Final Design for Architecture Project', subject: 'Architecture', dueDate: '12 May 2024', status: 'In Progress' }
+  //TAREAS EN TABLA
+  // COLUMNAS DE LA TABLA
+  columns = [
+    'ID',
+    'Tarea',
+    'Curso',
+    'Fecha de vencimiento',
+    'Estado'
+  ];
+  
+  // MAPEO PARA COLUMNAS Y FILAS
+  columnMappings = {
+    'ID': 'id',
+    'Tarea': 'task',
+    'Curso': 'subject',
+    'Fecha de vencimiento': 'dueDate',
+    'Estado': 'status'
+  };
+
+  rows: Assignment[] = [
+    { id: '01', task: 'Read Chapters 1-3', subject: 'English', dueDate: '12 May 2024', status: 'En progreso' },
+    { id: '02', task: 'Complete Problem Set #5', subject: 'Maths', dueDate: '12 May 2024', status: 'Completado' },
+    { id: '03', task: 'Write Lab Report on Acid-Base Titration', subject: 'Physics', dueDate: '12 May 2024', status: 'Pendiente' },
+    { id: '04', task: 'Prepare for Oral Presentation', subject: 'Chemistry', dueDate: '12 May 2024', status: 'Pendiente' },
+    { id: '05', task: 'Create Art Piece for Final Project', subject: 'English', dueDate: '12 May 2024', status: 'Pendiente' },
+    { id: '06', task: 'Write Research Paper on Climate Change', subject: 'EVS', dueDate: '12 May 2024', status: 'Completado' },
+    { id: '07', task: 'Complete Math Quiz on Algebra', subject: 'Math', dueDate: '12 May 2024', status: 'Completado' },
+    { id: '08', task: 'Prepare for History Class Debate', subject: 'History', dueDate: '12 May 2024', status: 'Pendiente' },
+    { id: '09', task: 'Submit Final Design for Architecture Project', subject: 'Architecture', dueDate: '12 May 2024', status: 'Completado' }
   ];
 
+  //HORARIO HOY
   todaySchedule = [
     { subject: 'Aritmética', time: '8:00 AM', color: '#fbbf24' },
     { subject: 'Inglés', time: '9:00 AM', color: '#a78bfa' },
