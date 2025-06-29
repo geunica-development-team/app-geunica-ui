1615aa3139426daeb75273e0edf7642ebd13e6d3
Kernar <kernarlarcerus6790@gmail.com>
2025-06-29 18:15:26 -0500

    buscador active sin pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/student/panel/courses/courses.component.ts
+++ src/app/student/panel/courses/courses.component.ts
@@ -2,19 +2,53 @@ import { Component, OnInit } from '@angular/core';
 import { CardCoursesComponent } from '../../../components/card-courses/card-courses.component';
 import { Course, DataService } from '../../services/dataStudent.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
-  standalone: true,
-  imports: [CardCoursesComponent, CommonModule],
+  
+  imports: [CardCoursesComponent, CommonModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
+
   courses: Course[] = [];
+  searchTerm: string = '';
 
   constructor(private dataSvc: DataService) {}    // <-- aquí
   ngOnInit() {
     this.dataSvc.getCourses().subscribe(c => this.courses = c);
   }
+
+  // getter que devuelve sólo los que coinciden con la busqeuda
+  get filteredCourses(): Course[] {
+    const raw = this.searchTerm.toLowerCase().trim();
+
+    // Si hay menos de 4 caracteres, no filtramos
+    if (raw.length < 4) {
+      return this.courses;
+    }
+
+    // 2) Normalizar y eliminar diacríticos del término
+    const normalizedSearch = raw
+      .normalize('NFD')                    // descompone caracteres acentuados
+      .replace(/[\u0300-\u036f]/g, '');    // elimina marcas diacríticas
+
+    // Separamos la búsqueda en palabras individuales
+    const terms = normalizedSearch.split(/\s+/);
+
+    // Para cada curso, comprobamos que todas las palabras aparezcan
+    return this.courses.filter(c => {
+      const title = c.title
+      .trim()
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+
+      // cada término debe aparecer en el título normalizado
+      return terms.every(t => title.includes(t));
+    });
+  }
+
 }
