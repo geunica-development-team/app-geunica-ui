89d4a0f9dbbfc317999f1fa3ec0346c6b154472a
Kernar <kernarlarcerus6790@gmail.com>
2025-07-05 23:14:05 -0500

    modal anuncio

--- src/app/student/panel/announcement/announcement.component.ts
+++ src/app/student/panel/announcement/announcement.component.ts
@@ -1,24 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Announcement, DataService } from '../../services/dataStudent.service';
 import { CommonModule } from '@angular/common';
-import { CardListComponent } from '../../../components/card-list/card-list.component';
-import { CardItemComponent } from '../../../components/card-item/card-item.component';
 import { map, Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from '../../../app.component';
+import { AppModalComponent } from '../../../components/app-modal/app-modal.component';
+
 
 @Component({
   selector: 'app-announcement',
-  imports: [CommonModule],
+  imports: [CommonModule, AppModalComponent],
   templateUrl: './announcement.component.html',
   styleUrl: './announcement.component.css'
 })
 export class AnnouncementComponent implements OnInit{
   
   announcements$!: Observable<Announcement[]>;
+  selectedAnnouncement: Announcement | null = null;
 
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
     this.announcements$ = this.dataService.getAnnouncements();
+  }
+
+    openAnnouncement(a: Announcement) {
+    this.selectedAnnouncement = a;
+  }
+
+  closeModal() {
+    this.selectedAnnouncement = null;
   }
 }
