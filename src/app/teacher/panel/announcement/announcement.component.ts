8ca915267fecc7fd940b33d952d86f328b8e0717
Kernar <kernarlarcerus6790@gmail.com>
2025-07-09 22:29:34 -0500

    searcher add notemangment

--- src/app/teacher/panel/announcement/announcement.component.ts
+++ src/app/teacher/panel/announcement/announcement.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Announcement, DataService } from '../../../student/services/dataStudent.service';
+import { CommonModule } from '@angular/common';
+import { AppModalComponent } from '../../../components/app-modal/app-modal.component';
 
 @Component({
   selector: 'app-announcement',
-  imports: [],
+  imports: [CommonModule, AppModalComponent],
   templateUrl: './announcement.component.html',
   styleUrl: './announcement.component.css'
 })
-export class AnnouncementComponent {
+export class AnnouncementComponent implements OnInit {
+  announcements$!: Observable<Announcement[]>;
+  selectedAnnouncement: Announcement | null = null;
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.announcements$ = this.dataService.getAnnouncements();
+  }
+
+    openAnnouncement(a: Announcement) {
+    this.selectedAnnouncement = a;
+  }
+
+  closeModal() {
+    this.selectedAnnouncement = null;
+  }
 }
--- src/app/teacher/panel/note-managment/note-managment.component.ts
+++ src/app/teacher/panel/note-managment/note-managment.component.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SearcherComponent } from '../../../components/searcher/searcher.component';
+import { DataService } from '../../../student/services/dataStudent.service';
 
 @Component({
   selector: 'app-note-managment',
-  imports: [],
+  imports: [CommonModule, SearcherComponent],
   templateUrl: './note-managment.component.html',
   styleUrl: './note-managment.component.css'
 })
 export class NoteManagmentComponent {
+  //filteredCourses: Course[] = [];
+  searchTerm: string = '';
 
+  constructor(private dataSvc: DataService) {}
+
+  ngOnInit() {
+   
+  }
+
+    onSearch() {
+    // opcional: aquí podrías disparar algún otro efecto al hacer submit,
+    // pero el filtrado ya lo maneja el (filtered)
+  }
 }
