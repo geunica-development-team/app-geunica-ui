5301f53f35c8925f2b0573eab759d9600c0299b3
depoo <paul.fcb55@gmail.com>
2025-07-16 08:28:43 -0500

    WIP: Cambios locales en dataTeacher.service.ts

--- src/app/teacher/services/dataTeacher.service.ts
+++ src/app/teacher/services/dataTeacher.service.ts
@@ -5,29 +5,11 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../enviroments/environment';
 
-export interface Sede {
-  id: number;
-  studentId: number;
-  division: 'primary' | 'secondary';
-  grade: string;
-  // agrega aquí otros campos que consideres
-}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataTeacherService {
   private base = environment.apiBase;
-  constructor(private http: HttpClient) {}
-
-  /** Obtener todas las sedes */
-  getSedes(): Observable<Sede[]> {
-    return this.http.get<Sede[]>(`${this.base}/sede`);
-  }
-
-  /** Obtener una sola sede por ID */
-  getSedeById(id: number): Observable<Sede> {
-    return this.http.get<Sede>(`${this.base}/sede/${id}`);
-  }
-
-}
+  constructor(private http: HttpClient) {}}
