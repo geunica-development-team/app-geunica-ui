eff007130632c23c4ce486e0e7ea2fb9f87b55bb
depoo <paul.fcb55@gmail.com>
2025-07-16 08:53:45 -0500

    calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { FullCalendarComponent } from '@fullcalendar/angular';
+import { FullCalendarModule } from '@fullcalendar/angular';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, FormsModule ],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, FormsModule, FullCalendarModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'app-geunica-ui';
+
 }
--- src/app/student/panel/schedule/schedule.component.ts
+++ src/app/student/panel/schedule/schedule.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
-import { FullCalendarComponent, FullCalendarModule } from '@fullcalendar/angular';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import dayGridPlugin  from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
+import { CommonModule } from '@angular/common';
+import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-schedule',
-  imports: [ FullCalendarModule],
+  imports: [ FullCalendarModule, CommonModule],
 
   templateUrl: './schedule.component.html',
   styleUrl: './schedule.component.css'
   })
 export class ScheduleComponent {
-  // Plugins que queremos usar
+  /* Plugins que queremos usar
   plugins = [ dayGridPlugin, timeGridPlugin, interactionPlugin ];
 
   // Eventos de ejemplo
@@ -31,5 +32,20 @@ export class ScheduleComponent {
       color: '#8e44ad'
     }
     // …añade más eventos según necesites
-  ];
+  ];*/
+
+    calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin, interactionPlugin],
+    dateClick: (arg) => this.handleDateClick(arg),
+    events: [
+      { title: 'event 1', date: '2019-04-01' },
+      { title: 'event 2', date: '2019-04-02' }
+    ]
+  };
+
+  handleDateClick(arg: DateClickArg) {
+    alert('date click! ' + arg.dateStr)
+  }
+
 }
