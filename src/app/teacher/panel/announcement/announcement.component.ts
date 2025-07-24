e4b23975569352dec1af389d48217d67b50bd7ad
kernar <kernarlarcerus6790@gmail.com>
2025-07-24 10:57:00 -0500

    quill rata

--- src/app/teacher/panel/announcement/announcement.component.ts
+++ src/app/teacher/panel/announcement/announcement.component.ts
@@ -1,25 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { AppModalComponent } from '../../../components/app-modal/app-modal.component';
 import { SearcherComponent } from '../../../components/searcher/searcher.component';
 import { Anuncio } from '../../services/modelTeacher';
 import { DataTeacherService } from '../../services/dataTeacher.service';
+import { NgSelectModule } from '@ng-select/ng-select';
+import Quill from 'quill';
 
 @Component({
   selector: 'app-announcement',
-  imports: [CommonModule, AppModalComponent],
+  standalone: true,
+  imports: [CommonModule, AppModalComponent, NgSelectModule],
   templateUrl: './announcement.component.html',
   styleUrl: './announcement.component.css'
 })
 export class AnnouncementComponent implements OnInit {
   announcements$!: Observable<Anuncio[]>;
   selectedAnnouncement: Anuncio | null = null;
+  creatingAnnouncement = false;
+  students: readonly any[]|null|undefined;
+
+
+  @ViewChild('editorContainer', { static: true }) editorContainer!: ElementRef<HTMLDivElement>;
+  private quillEditor!: Quill;//el quill no carga aaaaaah
 
   constructor(private dataService: DataTeacherService) {}
 
   ngOnInit() {
     this.announcements$ = this.dataService.getAnnouncements();
+  }
+
+
+    ngAfterViewInit() {
+    // Inicializa Quill editor con toolbar básico
+    this.quillEditor = new Quill(this.editorContainer.nativeElement, {
+      modules: {
+        toolbar: [ ['bold', 'italic', 'underline'], [{ list: 'ordered' }, { list: 'bullet' }], ['link'] ]
+      },
+      theme: 'snow'
+    });
   }
 
     openAnnouncement(a: Anuncio) {
@@ -28,5 +48,11 @@ export class AnnouncementComponent implements OnInit {
 
   closeModal() {
     this.selectedAnnouncement = null;
+    this.creatingAnnouncement = false;
   }
+
+  openModalAnuncio(){
+    this.creatingAnnouncement = true;
+  }
+
 }
