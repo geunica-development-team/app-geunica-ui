e0ab280df8d240298fc4853be0b6a374eff7bcef
Kernar <kernarlarcerus6790@gmail.com>
2025-07-23 23:55:09 -0500

    tabla pagos mejor

--- src/app/student/panel/payments/payments.component.ts
+++ src/app/student/panel/payments/payments.component.ts
@@ -14,8 +14,12 @@ interface ScheduleItem {
 })
 export class PaymentsComponent {
     schedule: ScheduleItem[] = [
-    { vencimiento: '2025-07-15', concepto: 'Cuota 1', accion: 'Pagar', monto: 400 },
+    { vencimiento: '2025-07-15', concepto: 'Cuota 1', accion: 'Pagado', monto: 400 },
     { vencimiento: '2025-08-15', concepto: 'Cuota 2', accion: 'Pagar', monto: 450 },
     { vencimiento: '2025-09-15', concepto: 'Cuota 3', accion: 'Pagar', monto: 500 },
+    { vencimiento: '2025-10-15', concepto: 'Cuota 4', accion: 'Pagar', monto: 500 },
+    { vencimiento: '2025-11-15', concepto: 'Cuota 5', accion: 'Pagar', monto: 500 },
+    { vencimiento: '2025-12-15', concepto: 'Cuota 6', accion: 'Pagar', monto: 500 },
+    { vencimiento: '2025-13-15', concepto: 'Cuota 7', accion: 'Pagar', monto: 500 },
   ];
 }
